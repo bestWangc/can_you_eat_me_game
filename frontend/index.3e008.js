cce8fea8977eb319cd71b155d4ef100167a69feb
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-10-30 17:17:40 +0800

    add share card

--- frontend/index.3e008.js
+++ frontend/index.3e008.js
@@ -1,4 +1,4 @@
-System.register(["./application.02994.js"], function (_export, _context) {
+System.register(["./application.d92a7.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
