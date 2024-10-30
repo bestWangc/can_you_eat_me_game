fb2e6837dcab99502426d272b924ed7b006069e6
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-10-30 10:51:44 +0800

    Fix getBag interface request multiple times

--- frontend/application.d5380.js
+++ frontend/application.d5380.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.b0ef3.json';
+          this.settingsPath = 'src/settings.45589.json';
           this.showFPS = false;
         }
         init(engine) {
--- frontend/index.f4085.js
+++ frontend/index.f4085.js
@@ -1,4 +1,4 @@
-System.register(["./application.6eafd.js"], function (_export, _context) {
+System.register(["./application.d5380.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
