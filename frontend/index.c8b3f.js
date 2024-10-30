5519fe0d3779a25522b0776d5b05d49ca50c383f
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-10-30 16:42:23 +0800

    add jwt request

--- frontend/index.c8b3f.js
+++ frontend/index.c8b3f.js
@@ -1,4 +1,4 @@
-System.register(["./application.eb118.js"], function (_export, _context) {
+System.register(["./application.02994.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
