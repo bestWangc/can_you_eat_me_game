d22c7109fd49a0d482a2ad706209718add610748
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-10-30 11:12:14 +0800

    get/post error  end game

--- frontend/index.e2bc8.js
+++ frontend/index.e2bc8.js
@@ -1,4 +1,4 @@
-System.register(["./application.b129a.js"], function (_export, _context) {
+System.register(["./application.eb118.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
