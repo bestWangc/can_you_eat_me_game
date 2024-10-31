d526b892cbc0c5bd5943a5660823ba483d8fcc32
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-10-31 14:27:35 +0800

    add  Invitation mechanism

--- frontend/index.4e463.js
+++ frontend/index.4e463.js
@@ -1,4 +1,4 @@
-System.register(["./application.d92a7.js"], function (_export, _context) {
+System.register(["./application.16aec.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
