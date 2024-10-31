028e689185b7698c16c62d27f8c14f6032e6333f
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-10-31 18:25:34 +0800

    Code Optimization

--- frontend/application.6131e.js
+++ frontend/application.6131e.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.0ec87.json';
+          this.settingsPath = 'src/settings.a2162.json';
           this.showFPS = false;
         }
         init(engine) {
--- frontend/index.3788a.js
+++ frontend/index.3788a.js
@@ -1,4 +1,4 @@
-System.register(["./application.0e706.js"], function (_export, _context) {
+System.register(["./application.6131e.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
