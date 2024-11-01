df93bbe25bc3ea812b51f0b212c12972e6074d73
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-11-01 11:17:24 +0800

    add box golds reward ui page

--- frontend/index.40b92.js
+++ frontend/index.40b92.js
@@ -1,4 +1,4 @@
-System.register(["./application.89fa4.js"], function (_export, _context) {
+System.register(["./application.db8f5.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
