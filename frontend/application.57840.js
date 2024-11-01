02f34bd5651e06649ddf8bb0944730ed21ed3ecf
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-11-01 09:15:43 +0800

    Code Optimization

--- frontend/application.57840.js
+++ frontend/application.57840.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.a2162.json';
+          this.settingsPath = 'src/settings.d4bf7.json';
           this.showFPS = false;
         }
         init(engine) {
