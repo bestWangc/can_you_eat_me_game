9d07ed2686a53555495558009f398bbcb0c607c9
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-11-01 20:33:25 +0800

    delete no use code

--- frontend/index.d3ad5.js
+++ frontend/index.d3ad5.js
@@ -1,4 +1,4 @@
-System.register(["./application.496c3.js"], function (_export, _context) {
+System.register(["./application.42bc2.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
