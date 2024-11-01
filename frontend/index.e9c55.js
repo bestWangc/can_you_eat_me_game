b50e47bc7d31b855a0c0dc980907f9cdcec876ba
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-11-01 14:08:38 +0800

    add 404 page

--- frontend/index.e9c55.js
+++ frontend/index.e9c55.js
@@ -1,4 +1,4 @@
-System.register(["./application.db8f5.js"], function (_export, _context) {
+System.register(["./application.a72f9.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
