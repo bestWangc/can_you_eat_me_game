c0e08a4acd42e2001715405393daea2b29f144ee
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-11-01 19:57:06 +0800

    alert update

--- frontend/index.9a6ac.js
+++ frontend/index.9a6ac.js
@@ -1,4 +1,4 @@
-System.register(["./application.6d05b.js"], function (_export, _context) {
+System.register(["./application.d1db7.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
