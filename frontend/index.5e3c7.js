0977cc29268136b41c9b3e23a69af08a3639a44b
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-11-04 17:37:02 +0800

    buff update

--- frontend/index.5e3c7.js
+++ frontend/index.5e3c7.js
@@ -1,4 +1,4 @@
-System.register(["./application.49eb8.js"], function (_export, _context) {
+System.register(["./application.b51d1.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
