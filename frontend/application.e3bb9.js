99a1e30acc18b7ba61559958f1d742868acb5b26
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-11-04 10:57:20 +0800

    ui update

--- frontend/application.e3bb9.js
+++ frontend/application.e3bb9.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.40099.json';
+          this.settingsPath = 'src/settings.74a50.json';
           this.showFPS = false;
         }
         init(engine) {
--- frontend/index.73d7d.js
+++ frontend/index.73d7d.js
@@ -1,4 +1,4 @@
-System.register(["./application.ea6a5.js"], function (_export, _context) {
+System.register(["./application.e3bb9.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
