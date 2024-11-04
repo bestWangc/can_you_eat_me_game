f1fd88db4f8fc97c04eb40ce3832143b22f8f602
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-11-04 14:37:25 +0800

    ui update

--- frontend/index.d17c2.js
+++ frontend/index.d17c2.js
@@ -1,4 +1,4 @@
-System.register(["./application.e3bb9.js"], function (_export, _context) {
+System.register(["./application.4c3c9.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
