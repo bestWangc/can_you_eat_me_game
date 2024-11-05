adf11c8855d3ea1bed6e519598f99d0df022d693
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-11-05 16:42:42 +0800

    ui update

--- frontend/index.068b0.js
+++ frontend/index.068b0.js
@@ -1,4 +1,4 @@
-System.register(["./application.6887d.js"], function (_export, _context) {
+System.register(["./application.d53dc.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
