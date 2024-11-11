cb3fad11ab591925ad7144abb57bac64fe6c31df
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-11-11 14:52:19 +0800

    bug fix

--- frontend/index.38ad5.js
+++ frontend/index.38ad5.js
@@ -1,4 +1,4 @@
-System.register(["./application.e250f.js"], function (_export, _context) {
+System.register(["./application.9175b.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
