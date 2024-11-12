759862919910de458abc44f733f01ba02789bfe9
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-11-12 18:45:58 +0800

    change sdk to tonweb

--- frontend/index.fae3b.js
+++ frontend/index.fae3b.js
@@ -1,4 +1,4 @@
-System.register(["./application.013d4.js"], function (_export, _context) {
+System.register(["./application.bce95.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
