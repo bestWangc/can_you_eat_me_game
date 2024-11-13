af5bbbcedb5b11c2c73505fa571c2e993f344653
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2024-11-13 15:11:42 +0800

    ui update

--- frontend/index.25d5c.js
+++ frontend/index.25d5c.js
@@ -1,4 +1,4 @@
-System.register(["./application.bce95.js"], function (_export, _context) {
+System.register(["./application.510b1.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
